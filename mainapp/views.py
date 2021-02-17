51c42af7786b3f4e9f97209a5e6db6a96e32ffda
Dmittorr <dmittorr@gmail.com>
2021-02-17 22:58:49 +0300

    Renew after security key changed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-
 # Create your views here.
 
-
-=======
-# Create your views here.
-
->>>>>>> a8a45b5351b6bef9480f13403f1c60f33bf79939
 def main(request):
     return render(request, "mainapp/index.html")
 
@@ -18,8 +11,4 @@ def products(request):
 
 
 def contact(request):
-<<<<<<< HEAD
     return render(request, "mainapp/contact.html")
-=======
-    return render(request, "mainapp/contact.html")
->>>>>>> a8a45b5351b6bef9480f13403f1c60f33bf79939
