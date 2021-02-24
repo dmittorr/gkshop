cc7a4dfcf73dc62b9b0c14eac05eae622ea4e88f
dmittorr <46674459+dmittorr@users.noreply.github.com>
2021-02-24 23:21:14 +0300

    Lesson 2 (#2)
    * Renew after security key changed
    
    * Lesson 2. Context transmission for templates
    
    * Lesson 2. Templates with extended context
    
    * Lesson 2. Templates with for-cycle to context
    
    * Lesson 2. Templates with for-cycle to context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,25 +1,86 @@
+import datetime
+
 from django.shortcuts import render
 
-<<<<<<< HEAD
-
 # Create your views here.
 
-
-=======
-# Create your views here.
-
->>>>>>> a8a45b5351b6bef9480f13403f1c60f33bf79939
 def main(request):
-    return render(request, "mainapp/index.html")
+    title = "главная"
+    products = [
+        {
+            "name": "Отличный рабочий стул",
+            "desc": "Удобно работать весь день",
+            "image_src": "product-1.jpg",
+            "image_href": "/product/1/",
+            "alt": "рабочий стул",
+        },
+        {
+            "name": "Стул с улучшенными характеристиками",
+            "desc": "Подойдёт для работы и отдыха",
+            "image_src": "product-2.jpg",
+            "image_href": "/product/2/",
+            "alt": "работа и отдых",
+        }
+    ]
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = "продукты"
+    links_menu = [
+        {"href": "products_all", "name": "Все товары"},
+        {"href": "products_home", "name": "Для дома"},
+        {"href": "products_office", "name": "В офис"},
+        {"href": "products_modern", "name": "Необычные"},
+        {"href": "products_classic", "name": "Классические"},
+    ]
+    same_products = [
+        {
+            "name": "Рабочий стул",
+            "desc": "Удобно весь день",
+            "image_src": "product-11.jpg",
+            "alt": "рабочий стул 11",
+        },
+        {
+            "name": "Улучшенный рабочий стул",
+            "desc": "Комфортно весь день",
+            "image_src": "product-21.jpg",
+            "alt": "рабочий стул 21",
+        },
+        {
+            "name": "Премиальный рабочий стул",
+            "desc": "Комфорт и отдых весь день",
+            "image_src": "product-31.jpg",
+            "alt": "рабочий стул 31",
+        }
+    ]
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-<<<<<<< HEAD
-    return render(request, "mainapp/contact.html")
-=======
-    return render(request, "mainapp/contact.html")
->>>>>>> a8a45b5351b6bef9480f13403f1c60f33bf79939
+    title = "О компании"
+    visit_date = datetime.datetime.now()
+    locations = [
+        {
+            "city": "Москва",
+            "phone": "+7 800 1234567890",
+            "e-mail": "info@gkshop.ru",
+            "address": "Не только в пределах МКАД",
+        },
+        {
+            "city": "Екатеринбург",
+            "phone": "+7 800 1234567891",
+            "e-mail": "info_ekaterinburg@gkshop.ru",
+            "address": "Центр России",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7 800 1234567892",
+            "e-mail": "infovladik@gkshop.ru",
+            "address": "Восточный край России",
+        },
+    ]
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
